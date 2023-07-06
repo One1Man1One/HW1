fbb881681c6ff8f1bec0c927182ee0f22628fa5f
«Mikhail» <moneydonay@gmail.com>
2023-07-06 22:34:25 +0300

    добавил решение задачи номер 4_2_3

--- Program.cs
+++ Program.cs
@@ -6,3 +6,10 @@ for(int i=1;i<=n;i++)
 if (i % 2 == 0)
 System.Console.Write(i + ", ");
 }
+//задание номер 3
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+System.Console.WriteLine("Число: " + a + " четное");
+else 
+System.Console.WriteLine($"Число: {a} нечетное");
